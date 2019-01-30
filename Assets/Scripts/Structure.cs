37c09c0bb6a9af0be480fb8e6a10909df3362a58
Alex M.A <alex.muab@gmail.com>
2019-01-30 23:33:20 +0100

    Add files via upload
    Adding unity project, Assets folder

---
+++ Assets/Scripts/Structure.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Structure : MonoBehaviour {
+    
+    /// <summary>
+    /// Splitting the mesh of the structure into triangles calling 
+    /// the static function Triangulate of MeshTriangulator and passing 
+    /// the structure transform
+    /// </summary>
+    void OnMouseDown(){
+        MeshTriangulator.Triangulate(transform);
+    }
+}
